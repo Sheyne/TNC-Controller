79d804b6f7d24554857ed11e83966b15ecd8d8b2
Sheyne <me@sheyne.com>
2011-08-03 17:55:20 -0600

    Initial Commit

---
+++ TNC Controller/TNC_ControllerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TNC_ControllerAppDelegate.h
+//  TNC Controller
+//
+//  Created by Sheyne Anderson on 8/3/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TNC_ControllerAppDelegate : NSObject <NSApplicationDelegate> {
+	NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TNC Controller/TNC_ControllerAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  TNC_ControllerAppDelegate.m
+//  TNC Controller
+//
+//  Created by Sheyne Anderson on 8/3/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#import "TNC_ControllerAppDelegate.h"
+
+@implementation TNC_ControllerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
